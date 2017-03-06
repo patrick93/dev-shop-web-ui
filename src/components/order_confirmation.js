bed9d6261c234c400f99a73b6dfef65c42c3996a
Patrick Gazaneo <pck1993@gmail.com>
2017-03-06 07:45:50 -0300

    improvement in order confirmation page

--- src/components/order_confirmation.js
+++ src/components/order_confirmation.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 const OrderConfirmation = () => {
+    let orderNumber = "";
+    for (var i = 0; i < 7; i++){
+        orderNumber += Math.floor(Math.random()*9);
+    }
     return (
         <div className="container">
             <h1>Thank You for your order</h1>
-            <h3>Order number is: 123456</h3>    
+            <h3>Order number is: {orderNumber}</h3>    
         </div>
     );
 };
