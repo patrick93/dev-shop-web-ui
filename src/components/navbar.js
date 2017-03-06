e3409aa82a30bef2a53bc0e1c26efb1f5b8247e6
Patrick Gazaneo <pck1993@gmail.com>
2017-03-06 10:01:28 -0300

    fix on the navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,16 +5,14 @@ const NavBar = () => {
     return (
         <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
             <div className="container">
-                <div className="navbar-header">
+                <div>
                     <Link to="/" className="navbar-brand">Dev Shop</Link>
                 </div>
-                <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul className="nav navbar-nav pull-right">
-                        <li>
-                            <Link to="/cart"><i className="fa fa-shopping-cart fa-2x"></i></Link>
-                        </li>
-                    </ul>
-                </div>
+                <ul className="nav navbar-nav pull-right">
+                    <li>
+                        <Link to="/cart"><i className="fa fa-shopping-cart fa-2x"></i></Link>
+                    </li>
+                </ul>
             </div>
         </nav>
     );
