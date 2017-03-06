192e663f700823c08a55f0ae3272aa861c83999a
Patrick Gazaneo <pck1993@gmail.com>
2017-03-06 10:08:42 -0300

    final adjustement

--- config/development.js
+++ config/development.js
@@ -1,7 +1,7 @@
 export default {
     CART_API_URL: "http://localhost:5000",
     GITHUB_AUTH: {
-        username: 'patrick93',
-        password: 'F76r161an196a'
+        username: '',
+        password: ''
     }
 }
